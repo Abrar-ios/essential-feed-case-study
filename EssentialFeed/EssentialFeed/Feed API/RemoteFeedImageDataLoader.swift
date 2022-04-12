82c02a33371a88fa85c255ab9f312387d02030ad
Abrar <abrar.haq@discorp.com>
2022-04-12 19:57:28 +0500

    RemoteFeedImageDataLoader requests data from URL on `loadImageDataFromURL`

---
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
@@ -0,0 +1,15 @@
+//
+//  RemoteFeedImageDataLoader.swift
+//  EssentialFeed
+//
+//  Created by Abrar on 12/04/2022.
+//  Copyright © 2022 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+class RemoteFeedImageDataLoader {
+    init(client: Any) {
+
+    }
+}
